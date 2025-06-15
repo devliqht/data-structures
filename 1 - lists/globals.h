192478135bc816d605a1a2342f8d95f601f7a620
Matt Cabarrubias <monapwc2020@gmail.com>
2025-06-16 00:50:25 +0800

    feat: add globals.h

---
+++ 1 - lists/globals.h
@@ -0,0 +1,24 @@
+
+#ifndef GLOBALS_H
+#define GLOBALS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_AMT 100
+#define MAX_STR_LEN 50
+
+#define INSERT_SUCCESS 1
+#define INITIALIZE_SUCCESS 1
+#define PRINT_SUCCESS 1
+#define INSERT_ERROR 0
+#define INITIALIZE_ERROR 0
+#define PRINT_ERROR 0
+#define NOT_FOUND -1
+#define LIST_FULL -2
+#define LIST_EMPTY -3
+
+typedef int status_t;
+
+#endif // GLOBALS_H
