86d0cc6db4f4f21cbf3d4cec1166aa8d9d942513
Matt Cabarrubias <monapwc2020@gmail.com>
2025-09-10 14:30:15 +0800

    add more stuff mwhehe

---
+++ 2 - stacks/activities/pairwise/main.c
@@ -0,0 +1,19 @@
+#include "headers/StacksLinked.h"
+// #include "headers/StacksArr.h"
+
+int main(void) {
+	Stack S;
+	init(&S);
+	push(&S, 'u');
+	push(&S, 's');
+	push(&S, 'c');
+	push(&S, 't');
+	push(&S, 'c');
+	printStack(&S);
+	printf("TOP ELEMENT: %c\n", top(&S));
+	pop(&S);
+	printStack(&S);
+	printf("TOP ELEMENT: %c\n", top(&S));
+	
+	return 0;
+}
